31e59c6820a224b2cf35d6db3905ba05a914e699
cpadridev <cpadridev@gmail.com>
2023-01-05 23:27:31 +0100

    Created the login

---
+++ tpv/tpv/Frontend/Dialogs/LoginDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace tpv.Frontend.Dialogs
+{
+    /// <summary>
+    /// Lógica de interacción para LoginDialog.xaml
+    /// </summary>
+    public partial class LoginDialog : MetroWindow
+    {
+        public LoginDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
